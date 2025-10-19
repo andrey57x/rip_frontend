349a43732cfa567cb1c2595e4b1f339522687e13
andrey57x <57andreys57@gmail.com>
2025-10-19 22:00:27 +0300

    not worling

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
-import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
-import AppNavbar from './components/layout/AppNavbar'
-import HomePage from './pages/HomePage'
-import ReactionsPage from './pages/ReactionsPage'
-import ReactionDetailPage from './pages/ReactionDetailPage'
-import './App.css'
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import AppNavbar from "./components/layout/AppNavbar";
+import HomePage from "./pages/HomePage";
+import ReactionsPage from "./pages/ReactionsPage";
+import ReactionDetailPage from "./pages/ReactionDetailPage";
+import "./App.css";
+import { ROUTER_BASENAME } from "./config/sharedConfig";
 
 const App: React.FC = () => {
   return (
-    <Router basename="/rip_frontend">
+    <Router basename={ROUTER_BASENAME}>
       <AppNavbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/reactions" element={<ReactionsPage />} />
         <Route path="/reactions/:id" element={<ReactionDetailPage />} />
-
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/config/sharedConfig.ts
+++ src/config/sharedConfig.ts
@@ -1,6 +1,20 @@
+declare const process: { env?: { TAURI_PLATFORM?: string } } | undefined;
+
+const IS_TAURI = typeof process !== "undefined" && !!process.env?.TAURI_PLATFORM;
+
+const LOCAL_BACKEND_IP = "http://89.208.210.115:8006";
+const LOCAL_MINIO_IP = "http://89.208.210.115:8001";
+
+const PUBLIC_BACKEND_URL = "https://reagent-mass-calculation.duckdns.org";
+const PUBLIC_MINIO_URL = "https://storage-reagent-mass.duckdns.org";
+
 export const FRONT_HOST = "localhost";
 export const FRONT_PORT = 3000;
 
-export const BACKEND_PATH = "https://reagent-mass-calculation.duckdns.org";
-export const API_BASE = BACKEND_PATH + "/api/v1";
+export const BACKEND_PATH = IS_TAURI ? LOCAL_BACKEND_IP : PUBLIC_BACKEND_URL;
+export const MINIO_PATH = IS_TAURI ? LOCAL_MINIO_IP : PUBLIC_MINIO_URL;
+
+export const ROUTER_BASENAME = IS_TAURI ? "/" : "/rip_frontend";
+
+export const API_BASE = `${BACKEND_PATH}/api/v1`;
 export const USE_MOCK = false;
